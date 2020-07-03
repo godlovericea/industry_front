bc23627d9c43a4c40a6357f021b0cbf6c5977d4f
godlovericea <1020097250@qq.com>
2020-07-03 16:52:36 +0800

    agre

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,43 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue"
+import Router from "vue-router"
 
-import dashboard from '../views/dashboard'
-import jurong from '../views/jurong'
-import fiveg from '../views/fiveg'
-import login from '../views/login/index'
+import dashboard from "../views/dashboard"
+import jurong from "../views/jurong"
+import fiveg from "../views/fiveg"
+import login from "../views/login/index"
+import agre from "../views/agre"
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: login
-    },
-    
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: dashboard
-    },
-    {
-      path: '/jurong',
-      name: 'jurong',
-      component: jurong
-    },
-    {
-      path: '/fiveg',
-      name: 'fiveg',
-      component: fiveg
-    }
-  ]
-}
-)
+    routes: [
+        {
+            path: "/",
+            redirect: "/login",
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: login,
+        },
+        {
+            path: "/agre",
+            name: "agre",
+            component: agre,
+        },
+        {
+            path: "/dashboard",
+            name: "dashboard",
+            component: dashboard,
+        },
+        {
+            path: "/jurong",
+            name: "jurong",
+            component: jurong,
+        },
+        {
+            path: "/fiveg",
+            name: "fiveg",
+            component: fiveg,
+        },
+    ],
+})
